d74c840047dbf6ca4615fd5005681a4206c10575
chit0804 <patelchit842005@gmail.com>
2026-02-10 17:59:32 +0530

    Update webhook_routes.py

--- backend/routes/webhook_routes.py
+++ backend/routes/webhook_routes.py
@@ -6,15 +6,13 @@ webhook_bp = Blueprint("webhook", __name__)
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 @webhook_bp.route("/webhook", methods=["GET", "POST"])
-def webhook():
+def whatsapp_webhook():
+
     # Meta verification
     if request.method == "GET":
         mode = request.args.get("hub.mode")
         token = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
-
-        print("VERIFY_TOKEN (server):", VERIFY_TOKEN)
-        print("VERIFY_TOKEN (meta):", token)
 
         if mode == "subscribe" and token == VERIFY_TOKEN:
             return challenge, 200
